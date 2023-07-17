8b54eb30c5ebd5fcd6d86030aec3eec4640d9a9d
xx <xx@gmail.com>
2023-07-17 16:06:13 +0800

    init

---
+++ app/src/main/java/org/cocos2dx/javascript/AppActivity.kt
@@ -0,0 +1,11 @@
+package org.cocos2dx.javascript
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AppActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
